70354d3357868ef71d157967c5bf53ac176b6a0d
Marie <mcari1311@gmail.com>
2022-08-05 12:23:47 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express')
+// const { default: mongoose } = require('mongoose')
 require('dotenv').config() 
 const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override');
 const port = process.env.PORT || 3003
+
+//mongoose connect
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 //view engine 
 app.set('view engine', 'jsx') 
 app.engine('jsx', require('express-react-views').createEngine())
 
-//try route
+//middleware
+app.use((req, res, next) => {
+	console.log('I run for all routes')
+	next()
+}) 
+app.use(express.urlencoded({extended:false}));
+//method override
+app.use(methodOverride('_method'));
+
+
+//test
 app.get('/', (req, res) => {
     res.send('hello')
 })
+
 
 app.listen(port, function() {
     console.log('Listening on port', port)
